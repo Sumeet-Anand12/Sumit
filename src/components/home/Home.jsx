c96d767ac62a51db4293bed0a9756300dea5e66b
Sumit Anand <sumitanand287@gmail.com>
2025-03-08 01:29:27 +0530

    change

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import Rotation from './loader/Rotation'
 import CategoryPanel from './sidebarPanel/CategoryPanel';
 import Banner from './banner/Banner';
 import HomeCatSlider from './slider/HomeCatSlider';
+import CardBanner from './cardbanner/CardBanner';
 
 const HomePage = () => {
   return (
@@ -11,6 +12,9 @@ const HomePage = () => {
         <Rotation />
         <Banner />
         <HomeCatSlider />
+      </div>
+      <div className='home-card-banner'>
+        <CardBanner />
       </div>
     </div>
   )
