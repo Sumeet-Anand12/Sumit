061c89d8f240e629ffe8a5c8dac661648abb6dc8
Sumit Anand <sumitanand287@gmail.com>
2025-03-14 01:16:47 +0530

    cateTab

--- src/components/home/categoryTab/Electronic.jsx
+++ src/components/home/categoryTab/Electronic.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Navigation } from 'swiper/modules';
+import CateCard from './CateCard';
 
 const Electronic = () => {
   return (
-    <div>Electronic</div>
+    <div className=' pt-4'>
+      <Swiper
+        navigation={true}
+        slidesPerView={6}
+        spaceBetween={0}
+        modules={[Navigation]}
+        className="mySwiper Category-wrapper"
+      >
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/103-large_default/pack-mug-framed-poster.jpg"/>
+        </SwiperSlide>
+      </Swiper>
+
+
+    </div>
   )
 }
 
--- src/components/home/categoryTab/Furniture.jsx
+++ src/components/home/categoryTab/Furniture.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectFade, Navigation } from 'swiper/modules';
+import CateCard from './CateCard';
+
 
 const Furniture = () => {
   return (
-    <div>Furniture</div>
+    <div className=' pt-4'>
+      <Swiper
+        navigation={true}
+        slidesPerView={6}
+        spaceBetween={0}
+        modules={[Navigation]}
+        className="mySwiper Category-wrapper"
+      >
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CateCard img="https://demos.codezeel.com/prestashop/PRS21/PRS210502/36-large_default/the-best-is-yet-to-come-framed-poster.jpg" />
+        </SwiperSlide>
+      </Swiper>
+
+
+    </div>
   )
 }
 
